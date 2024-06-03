1430923a001739f9ed5b61cc1b429b9d02ce9014
FBDev64 <elzadam11@gmail.com>
2024-06-03 17:46:50 +0100

    Server

--- src/server/server.sh
+++ src/server/server.sh
@@ -9,21 +9,20 @@ FILE_LOG="files/file_log.txt"
 
 # Start the server
 nc -l -p $SERVER_PORT -k > "$MESSAGE_LOG" &
-echo "Server started on port $SERVER_PORT. Logs are being written to $MESSAGE_LOG."
+gum log --level info "Server started on port $SERVER_PORT. Logs are being written to $MESSAGE_LOG."
 
 while true; do
     action=$(gum choose "View Logs" "Stop Server" "Exit")
     case $action in
         "View Logs")
-            gum pager --header "Server Logs" --content "$(cat "$MESSAGE_LOG")"
+            gum pager < server.log
             ;;
         "Stop Server")
             kill $(jobs -p)
-            echo "Server stopped."
+            gum log --level info "Server stopped."
             exit 0
             ;;
         "Exit")
-            kill $(jobs -p)
             exit 0
             ;;
     esac
