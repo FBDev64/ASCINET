10b23846edf40bd242034182b958860264508427
FBDev64 <elzadam11@gmail.com>
2024-06-08 15:45:53 +0100

    Resolved bugs with ssh

--- src/supervisor/supervisor.sh
+++ src/supervisor/supervisor.sh
@@ -2,7 +2,8 @@
 
 # Server address and port
 
-SERVER_HOSTNAME=$(gum input --placeholder="user@ip")
+SERVER_USER=$(gum input --placeholder="Remote user")
+SERVER_IP=$(gum input --placeholder="Remote IP")
 SERVER_PORT=8080
 
 # List of authorized supervisor computers
@@ -10,11 +11,11 @@ SUPERVISORS=("fbdev")
 SUPERVISOR_PASSWORD="3310"
 
 # Connect to the server
-exec 3<>/dev/tcp/$SERVER_HOSTNAME/$SERVER_PORT || exit 1
-gum log --level info "Connected to $SERVER_HOSTNAME:$SERVER_PORT" >&2
+exec 3<>/dev/tcp/$SERVER_IP/$SERVER_PORT || exit 1
+gum log --level info "Connected to $SERVER_USER@$SERVER_IP:$SERVER_PORT" >&2
 
 # Authenticate supervisor
-username=$(gum input --placeholder="Enter username: ")
+username=$(gum input --placeholder="Enter Supervisor Username")
 is_supervisor=false
 for supervisor in "${SUPERVISORS[@]}"; do
     if [ "$supervisor" == "$username" ]; then
@@ -24,7 +25,7 @@ for supervisor in "${SUPERVISORS[@]}"; do
 done
 
 if $is_supervisor; then
-    password=$(gum input --password --placeholder="Enter password: ")
+    password=$(gum input --password --placeholder="Enter Supervisor Password")
     if [ "$password" == "$SUPERVISOR_PASSWORD" ]; then
         echo "Supervisor $username authenticated."
     else
@@ -38,7 +39,7 @@ while true; do
     action=$(gum choose "Alert" "Send Message" "Send File" "View Files" "Help" "Quit")
     case "$action" in
         "Alert")
-            ALERT_MSG = $(gum input --placeholder="Alert Message: ")
+            ALERT_MSG=$(gum input --placeholder="Alert Message: ")
             gum log --level warn "$ALERT_MSG"
             ;;
         "Send Message")
@@ -48,11 +49,11 @@ while true; do
             ;;
         "Send File")
             file_path=$(gum file "$(pwd)")
-            gum log --level info "Wrote file at remote server in /files/ folder." | ssh "$SERVER_HOSTNAME" -p "$SERVER_PORT" -T "cat > /file/"$file_path""
+            gum log --level info "Wrote file at remote server in /files/ folder." | ssh "$SERVER_USER"@"$SERVER_IP" -p "$SERVER_PORT" -T "cat > /file/"$file_path""
             ;;
         "View Files")
-            chosen = $(gum file --all $SERVER_ADDR)
-            gum pager > $chosen
+            ssh "$SERVER_HOSTNAME" -p "$SERVER_PORT" -T "cat > /file/"$file_path"" | chosen = $(gum file --all $SERVER_ADDR)
+            gum pager < "$chosen"
             ;;
         "Help")
             glow github.com/FBDev64/ASCINET/
