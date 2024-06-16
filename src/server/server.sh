234eba10e3fe50f4da196fe9a6bbbaa8eccc9566
FBDev64 <elzadam11@gmail.com>
2024-06-16 19:52:10 +0100

    smol editz'

--- src/server/server.sh
+++ src/server/server.sh
@@ -7,9 +7,8 @@ SERVER_PORT=8080
 MESSAGE_LOG="server.log"
 
 # Start the Soft-Serve server
-soft serve
-
 gum log --level info "Server started on port $SERVER_PORT. Logs are being written to $MESSAGE_LOG."
+soft serve
 
 # Function to display logs
 display_logs() {
@@ -17,18 +16,5 @@ display_logs() {
 }
 
 while true; do
-    action=$(gum choose "View Logs" "Stop Server" "Exit")
-    case $action in
-        "View Logs")
-            display_logs
-            ;;
-        "Stop Server")
-            kill $(jobs -p)
-            gum log --level info "Server stopped."
-            exit 0
-            ;;
-        "Exit")
-            exit 0
-            ;;
-    esac
+    display_logs
 done
